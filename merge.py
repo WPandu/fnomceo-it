63deeec1bd59f70ff01fac03cf72eb644b2be551
Pandu Warjaya <panduwarjaya@gmail.com>
2025-08-06 15:16:12 +0700

    add ids

--- merge.py
+++ merge.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob
 
 # List all Excel files in the current directory
-excel_files = glob.glob('results/*.xlsx')
+excel_files = glob.glob('ids/*.xlsx')
 
 # Create an empty list to store dataframes
 dfs = []
@@ -20,6 +20,6 @@ chunk_size = 999998
 chunks = [merged_df[i:i+chunk_size] for i in range(0, len(merged_df), chunk_size)]
 
 # Save each chunk to a new sheet in the output Excel file
-with pd.ExcelWriter('results/results.xlsx', engine='openpyxl') as writer:
+with pd.ExcelWriter('ids/ids.xlsx', engine='openpyxl') as writer:
     for i, chunk in enumerate(chunks):
         chunk.to_excel(writer, sheet_name=f'Sheet{i+1}', index=False)
